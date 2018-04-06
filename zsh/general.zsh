ce2c38894745be2e27765a4b3031b0b3b4a6d52b
Dzmitry Hubin (EPAM) <dhubin.contractor@libertyglobal.com>
2018-04-06 15:38:15 +0200

    Use maven plugin for oh-zsh

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -52,7 +52,7 @@ ZSH_THEME="powerlevel9k/powerlevel9k"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git mvn)
 
 source $ZSH/oh-my-zsh.sh
 
