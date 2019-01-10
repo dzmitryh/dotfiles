8dbc20fd2b537d02894ebd629d230acf96b4979b
Dzmitry Hubin (EPAM) <dhubin.contractor@libertyglobal.com>
2019-01-10 18:57:59 +0100

    Add tools file with sdkman setup

---
+++ zsh/tools.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/Users/dzmitry/.sdkman"
+[[ -s "/Users/dzmitry/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/dzmitry/.sdkman/bin/sdkman-init.sh"
