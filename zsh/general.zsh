17ba5f62691643f54d04818a76dda18497d7d16c
Dmitry Gubin <dgubin@productiveedge.com>
2021-11-25 18:47:53 +0300

    add nvm specifics && use gradle

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -52,7 +52,7 @@ ZSH_THEME="powerlevel9k/powerlevel9k"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git mvn)
+plugins=(git gradle)
 
 source $ZSH/oh-my-zsh.sh
 [[ /usr/local/bin/kubectl ]] && source <(kubectl completion zsh)
